280520c96df44b1c06008724faabd3f8793fb84a
Logan Bernard <bernard.logan4@gmail.com>
2014-11-05 14:22:19 -0500

    project management in progress

--- marketUI/views.py
+++ marketUI/views.py
@@ -9,8 +9,9 @@ def create_user(request):
 	return render(request, 'create_user.html', {'register': 'create new user page.'})
 
 def projects(request):
-	projects = [{'name':'Project1', 'desc':'This is my first project.'}, 
-	{'name': 'Project2', 'desc':'I should have a better name.' }]
+	projects = [
+		{'name':'Project1', 'desc':'This is my first project.'}, 
+		{'name': 'Project2', 'desc':'I should have a better name.' } ]
 
 	return render(request, 'projects.html', {'user_projects': projects})
 
@@ -18,7 +19,11 @@ def market(request):
 	return render(request, 'market.html', {'market': 'marketplace page.'})
 
 def manage(request):
-	return render(request, 'manage.html', {'manage': 'project management page.'})
+	VMs = [
+		{'name': 'VM1', 'fields': {'compute':'BU-large', 'storage': 'EMC-small', 'image': 'CentOS', 'status': 'off'}}, 
+		{'name': 'VM2', 'fields': {'compute':'HU-small', 'storage': 'HP-medium', 'image': 'Ubuntu', 'status': 'off'}} ]
+
+	return render(request, 'manage.html', {'project_VMs': VMs})
 
 def settings(request):
 	return render(request, 'settings.html', {'settings': 'project settings page.'})
