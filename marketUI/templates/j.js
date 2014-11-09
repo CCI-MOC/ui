ffea9702b2614d913ebfbacf7359074c0e77f842
Lucid Structure <shiran@early-adopter.com>
2014-11-09 12:25:09 -0500

    added filter

---
+++ marketUI/templates/j.js
@@ -0,0 +1,21 @@
+//MARKETPLACE JS
+$( document ).ready(function() {
+    $('.item').hide()
+    $('.service').show()
+    console.log("shishi")
+    $('.market-nav').click(function() {
+        $('.a').removeClass('a')
+        $(this).addClass('a')
+        var n = $(this).attr('name')
+        console.log(n)
+        $('.item').hide()
+        $('.'+n).show()
+    })
+
+    $('.cart').click(function() {
+        console.log($(this).siblings('td'))
+        jQuery.noConflict();
+        $('#myModal').modal('show')
+
+    })
+});
