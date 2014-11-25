e6a9c8f5756878ffc71a4c64341515257640beec
logan <bernard.logan4@gmail.com>
2014-11-25 11:49:50 -0500

    working list, needs editing

--- marketUI/ui_api.py
+++ marketUI/ui_api.py
@@ -1,30 +1,23 @@
-import settings
+from settings import Nova
 
-def list(): 
-
-	nova = settings.Nova
+def list():
 	vms = []
-	server_list = nova.servers.list()
+	server_list = Nova.servers.list(detailed=True)
 	for server in server_list:
-		vm = {'name': server.name,'image':server.image[u'id']}
+		vm = {'name':server.name, 'desc':server.id, 'status':server.status, 'image':server.image[u'id'], 'network':server.networks[u'private']}
 		vms.append(vm)
 	return vms
+list()
 
 def create(): 
-	#nova = Client('2', 'nova', 'admin', 'service', 'http://10.0.2.15:5000/v2.0')
-	nova = settings.Nova
 	name = raw_input('name: ')
 	fl   = raw_input('flavor: ')  
-	fl = nova.flavors.find(name='m1.'+fl)
-	print(nova.servers.create(name, 'ed451e82-887b-42e1-b631-d28f46a5eed2',flavor=fl,meta=None,files=None))
+	fl = Nova.flavors.find(name='m1.'+fl)
+	print(Nova.servers.create(name, 'ed451e82-887b-42e1-b631-d28f46a5eed2',flavor=fl,meta=None,files=None))
 
-
-import time
 
 def delete(): 
-	#nova = Client('2', 'nova', 'admin', 'service', 'http://10.0.2.15:5000/v2.0')
-	nova = settings.Nova
-	servers_list = nova.servers.list()
+	servers_list = Nova.servers.list()
 	server_exists = False
         name = raw_input('name: ')
 	for s in servers_list:
@@ -39,4 +32,3 @@ def delete():
 	    nova.servers.delete(s)
 	    print("server %s deleted" % name)	
 
-list()
